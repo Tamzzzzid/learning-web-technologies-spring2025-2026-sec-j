5fe7146c8fd0a428c81d43ab1fd386db7f32124f
Tamzzzzid <tamzid0172@gmail.com>
2026-04-12 14:46:10 +0600

    Add files via upload
    TAS2_A

---
+++ FINAL LAB TASK 1/task2/taskA/email.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="val.php" method="post">
+        <fieldset>
+            <legend>Email</legend>
+             <input type="text" name="user">
+             <hr>
+             <input type="submit" name="submit">
+        </fieldset>
+    </form>
+</body>
+</html>
