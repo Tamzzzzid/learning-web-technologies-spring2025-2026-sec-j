dc7885767b1c5ad9cc6c689bc61d0d2ba6a7ae6a
Tamzzzzid <tamzid0172@gmail.com>
2026-04-12 21:32:14 +0600

    task4c

---
+++ FINAL LAB TASK 1/task4/TASKC/form.php
@@ -0,0 +1,31 @@
+<?php 
+$gender = $_POST['gender'] ?? '';
+
+if(isset($_POST["submit"])){
+    if($gender == ''){
+        echo "No gender selected.";
+    } else {
+        echo "Selected Gender: {$gender}";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Gender Form</title>
+</head>
+<body>
+    <fieldset style="width:25%">
+        <legend>GENDER</legend>
+        <form action="" method="post">
+          <input type="radio" name="gender" value="Male" <?php if($gender == 'Male') echo 'checked'; ?>> Male
+          <input type="radio" name="gender" value="Female" <?php if($gender == 'Female') echo 'checked'; ?>> Female
+          <input type="radio" name="gender" value="Other" <?php if($gender == 'Other') echo 'checked'; ?>> Other
+          <hr>
+          <input type="submit" name="submit" value="Submit">
+        </form>
+    </fieldset>
+</body>
+</html>
