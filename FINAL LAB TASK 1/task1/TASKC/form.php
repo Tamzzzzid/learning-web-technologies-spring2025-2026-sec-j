c02ecb942d7a1699d5a89ed110545f4af70c1df9
Tamzzzzid <tamzid0172@gmail.com>
2026-04-12 14:35:21 +0600

    task1done

---
+++ FINAL LAB TASK 1/task1/TASKC/form.php
@@ -0,0 +1,26 @@
+<?php 
+    if(isset($_REQUEST['submit'])){
+            //print_r($_GET);
+
+            $username = $_REQUEST['user'];
+            
+            if(isset($_REQUEST['user'])){
+                echo $username;}
+            } ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <fieldset>
+            <legend>Name</legend>
+            <input type="text" name="user" value="<?php echo isset($_REQUEST['user']) ? $_REQUEST['user'] : ''; ?>">             <hr>
+             <input type="submit" name="submit">
+        </fieldset>
+    </form>
+</body>
+</html>
---
+++ FINAL LAB TASK 1/task1/taskA/val.php
@@ -0,0 +1,9 @@
+<?php 
+    if(isset($_REQUEST['submit'])){
+            //print_r($_GET);
+
+            $username = $_REQUEST['user'];
+            
+            if(isset($_REQUEST['user'])){
+                echo $username;}
+            } ?>
