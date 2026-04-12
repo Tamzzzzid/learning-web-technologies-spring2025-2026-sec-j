03b7f8b15b65f11d52e2ef19024da9a4dd3b46b3
Tamzzzzid <tamzid0172@gmail.com>
2026-04-12 19:45:42 +0600

    Delete FINAL LAB TASK 1 directory

--- FINAL LAB TASK 1/TASK1/val.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    if(isset($_REQUEST['submit'])){
-            //print_r($_GET);
-
-            $username = $_REQUEST['user'];
-            
-            if(isset($_REQUEST['user'])){
-                echo $username;}
-            } ?>
